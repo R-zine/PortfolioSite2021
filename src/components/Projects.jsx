058e1079e9705a0e7c363a38902bb7ab57a746bf
Ivan Radev <107320114+R-zine@users.noreply.github.com>
2023-07-08 18:56:50 +0300

    Update projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,6 +7,12 @@ import "./project.css";
 const Projects = () => {
   const Projects = [
     new Proj(
+      "./img/portfolio2023.jpg",
+      "https://ivanradev.netlify.app/",
+      "https://github.com/R-zine/Portfolio-2023",
+      `The new version of this website. Made entirely in 3D with React-Fiber with Drei and Rapier physics, powered by Redux Toolkit and GSAP.`
+    ),
+    new Proj(
       "./img/stacker.jpg",
       "https://stckr.netlify.app/",
       "https://github.com/R-zine/stackr",
@@ -15,30 +21,35 @@ const Projects = () => {
       The game saves the local highscore to localStorage.`
     ),
     new Proj(
+      "./img/art.jpg",
+      "https://ivanradevart.netlify.app",
+      "https://github.com/R-zine/personal-paintings-website",
+      "My personal traditional paintings shop. Built on React with Redux Toolkit. Animations are handled by GSAP and React Spring. The site uses custom Wordpress API/CMS."
+    ),
+    new Proj(
+      "./img/snake.jpg",
+      "https://next-snake-r-zine.vercel.app/",
+      "https://github.com/R-zine/snake-next-js",
+      "This is the classic snake game made as a full-stack NextJS app with Prisma for backend/DB management and PostgreSQL for saving user profiles, settings, and highscores."
+    ),
+    new Proj(
+      "./img/native.jpg",
+      "https://appetize.io/embed/wmt2t4k2p3agh4tttqwpm3ncpy",
+      "https://github.com/R-zine/ItemHunter",
+      "An AI-powered game in which the user gets points when they find one of 300 items in the real world. This App is built on React Native (Expo) and has TensorFlowJS object recognition. The score and current selection are handled via Redux with persistors. The built .APK is available in the GitHub repository."
+    ),
+    new Proj(
+      "./img/arch.jpg",
+      "https://dradeva.netlify.app/",
+      "https://github.com/R-zine/architect-portfolio",
+      "An architectural portfolio website, featuring many different types of animations and an unique styling approach. The website is fully responsive and has a custom 3D model in the contacts' page."
+    ),
+    new Proj(
       "./img/mintex.jpg",
       "https://mintex.netlify.app",
       "https://github.com/R-zine/mintex",
       "A Web 3.0 minting site built on React/Vite using HardHat, Solidity, and featuring MetaMask wallet integration."
     ),
-    new Proj(
-      "./img/art.jpg",
-      "https://ivanradevart.netlify.app",
-      "https://github.com/R-zine/art",
-      "My personal traditional paintings shop. Built on React/Router and Commerce.js. Payments are handled through Stripe."
-    ),
-    new Proj(
-      "./img/snake.jpg",
-      "https://r-zine.github.io/snake/",
-      "https://github.com/R-zine/snake",
-      "This is the classic snake game, featuring localStorage memory for user settings as well as highscore."
-    ),
-    new Proj(
-      "./img/hangman.jpg",
-      "https://r-zine.github.io/hangman/",
-      "https://github.com/R-zine/hangman",
-      "A hangman game with an on-screen keyboard, fetching random words off of an API. The right answer can be found in the console."
-    ),
-
     new Proj(
       "./img/wiki.jpg",
       "https://r-zine.github.io/wiki-search/",
